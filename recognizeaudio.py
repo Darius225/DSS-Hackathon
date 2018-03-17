6ccc1e902ffdddd1319622145b6e2788fbe5ac3d
Darius <darius.grigore.pop10@gmail.com>
2018-03-17 14:00:15 +0000

    Tried
    Did it

--- recognizeaudio.py
+++ recognizeaudio.py
@@ -24,11 +24,30 @@ def get_text( AUDIO_PATH):
 def extract_keywords( text ):
 	r = Rake()
 	r.extract_keywords_from_text(text)
-	print(r.get_ranked_phrases())
+	return r.get_ranked_phrases()
+
+def stop_keywords( word ):
+	config = Decoder.default_config()
+	config.set_string('-keyphrase', word)
+	config.set_float('-kws_threshold', 1e-30)
+	stream = open(path.join(path.dirname(path.realpath(__file__)), "audio.wav" ), "rb")
+	decoder = Decoder(config)
+	decoder.start_utt()
+	while True:
+		buf = stream.read(1024)
+		if buf:
+			decoder.process_raw(buf, False, False)
+		else:
+			break
+		if decoder.hyp() != None:
+			print ([(seg.word, seg.prob, seg.start_frame, seg.end_frame) for seg in decoder.seg()])
+			print ("Detected keyphrase, restarting search")
+			decoder.end_utt()
+			decoder.start_utt()
 
 text =get_text("audio.wav")
 print(text)
-extract_keywords(text)
+stop_keywords("work")
 #command = "ffmpeg -i c.mp4 -ab 160k -ac 2 -ar 44100 -vn audio.wav"
 #subprocess.call(command, shell=True)
 #AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "audio.wav")
